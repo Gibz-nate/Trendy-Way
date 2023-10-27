87ebc79fd986b929176b28d89740b060d6e47059
Gibz Nate <gibsonate123@gmail.com>
2023-10-27 16:56:58 +0300

    debug

--- trendyway/settings.py
+++ trendyway/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yf8iop+s6wk*)c54o^&q(ny(r_@dvq!yb_x+8b3pj6z$w7-r8@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app',
                 '127.0.0.1',
